d27125905ebcfcdec1c4e91a5c5418c0685348e4
brandon <gwonzhang@gmail.com>
2023-05-31 15:05:11 +0800

    add basic logic for metronome

--- src/components/guitar-metronome/index.js
+++ src/components/guitar-metronome/index.js
@@ -1,17 +1,129 @@
+// @ts-check
 import css from "./style.css?inline";
+import BeatStart from "@sounds/beat-start.wav";
+import BeatNormal from "@sounds/beat-normal.wav";
 
+/**
+ * A custom element that provides a metronome for guitar practice.
+ * @extends HTMLElement
+ */
 export default class GuitarMetronome extends HTMLElement {
+  /**
+   * The beats per minute (BPM) of the metronome.
+   * @type {number}
+   * @private
+   */
+  #bpm;
+  
+  /**
+   * A flag indicating whether the metronome is currently playing.
+   * @type {boolean}
+   * @private
+   */
+  #isPlaying;
+
+  /**
+   * The ID of the interval timer used to play the metronome.
+   * @type {number} 
+   * @private
+   */
+  #timerId;
+
+  /**
+   * The time signature of the metronome, expressed as a string
+   * in the format "X/Y"
+   * @type {string} 
+   * @private
+   */
+  #signature;
+
+  /**
+   * The root element of the shadowDOM
+   * @type {ShadowRoot} 
+   * @private
+   */
+  #root;
+
+  /**
+   * Creates a new instance of the GuitarMetronome class.
+   */
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
+    this.root = this.attachShadow({ mode: "open" });
   }
 
   connectedCallback() {
+    const bpmAttr = this.getAttribute('npm');
+    if (bpmAttr !== null) {
+      const bpm = parseInt(bpmAttr, 10);
+      if (bpm <= 0 || isNaN(bpm)) {
+        return console.error(`Invalid value for "bpm" attribute. Must be a number greater than 0.`);
+      }
+      this.#bpm = bpm;
+    } else {
+      this.#bpm = 80;
+    }
+
+    this.#isPlaying = false;
+    this.#timerId = null;
+
     this.render();
   }
 
+  /**
+   * Starts the metronome.
+   */
+  start() {
+    if (this.#isPlaying) {
+      return;
+    }
+
+    console.log(1)
+
+    /** @type {number} */
+    let beatCount = 0;
+
+    const interval = 60000 / this.#bpm;
+    this.#timerId = setInterval(() => {
+      // Play the metronome sound
+      const audio = new Audio(beatCount % 4 === 0 ? BeatStart : BeatNormal);
+      audio.play();
+      beatCount++;
+    }, interval);
+
+    this.#isPlaying = true;
+  }
+
+  /**
+   * Stops the metronome.
+   */
+  stop() {
+    if (!this.#isPlaying) {
+      return;
+    }
+
+    clearInterval(this.#timerId);
+    this.#timerId = null;
+  }
+
+  /**
+   * Sets the BPM of the metronome.
+   * @param {number} bpm - The new BPM value.
+   */
+  setBpm(bpm) {
+    if (bpm <= 0 || isNaN(bpm)) {
+      throw new Error('Invalid BPM value. Must be a number greater than 0.');
+    }
+
+    this.#bpm = bpm;
+    if (this.#isPlaying) {
+      this.stop();
+      this.start();
+    }
+  }
+
   render() {
-    this.shadowRoot.innerHTML = `
+    this.root.innerHTML = `
       <style>${css}</style>
       <div class="signature">
         <button class="signature" data-signature="3/4">
@@ -35,6 +147,9 @@ export default class GuitarMetronome extends HTMLElement {
         <div class="beat"></div>
         <div class="beat"></div>
         <div class="beat"></div>
+      </div>
+      <div class="controls">
+        <button class="control start" aira-label="Start the metronome" onclick="this.getRootNode().host.start()">Start</button>
       </div>
       <div class="controls">
         <button class="control decrement" aria-label="Decrease tempo">
