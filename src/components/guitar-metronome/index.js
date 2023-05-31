21d188ec81437c0f06f112309070e8972d190dca
brandon <gwonzhang@gmail.com>
2023-06-01 00:33:30 +0800

    improve logic

--- src/components/guitar-metronome/index.js
+++ src/components/guitar-metronome/index.js
@@ -14,7 +14,7 @@ export default class GuitarMetronome extends HTMLElement {
    * @private
    */
   #bpm;
-  
+
   /**
    * A flag indicating whether the metronome is currently playing.
    * @type {boolean}
@@ -24,7 +24,7 @@ export default class GuitarMetronome extends HTMLElement {
 
   /**
    * The ID of the interval timer used to play the metronome.
-   * @type {number} 
+   * @type {number}
    * @private
    */
   #timerId;
@@ -32,14 +32,14 @@ export default class GuitarMetronome extends HTMLElement {
   /**
    * The time signature of the metronome, expressed as a string
    * in the format "X/Y"
-   * @type {string} 
+   * @type {string}
    * @private
    */
   #signature;
 
   /**
    * The root element of the shadowDOM
-   * @type {ShadowRoot} 
+   * @type {ShadowRoot}
    * @private
    */
   #root;
@@ -53,11 +53,13 @@ export default class GuitarMetronome extends HTMLElement {
   }
 
   connectedCallback() {
-    const bpmAttr = this.getAttribute('npm');
+    const bpmAttr = this.getAttribute("npm");
     if (bpmAttr !== null) {
       const bpm = parseInt(bpmAttr, 10);
       if (bpm <= 0 || isNaN(bpm)) {
-        return console.error(`Invalid value for "bpm" attribute. Must be a number greater than 0.`);
+        return console.error(
+          `Invalid value for "bpm" attribute. Must be a number greater than 0.`,
+        );
       }
       this.#bpm = bpm;
     } else {
@@ -71,14 +73,31 @@ export default class GuitarMetronome extends HTMLElement {
   }
 
   /**
+   * Toggles the metronome play state.
+   * @param {HTMLButtonElement} button
+   */
+  toggle(button) {
+    const label = button.querySelector(".sr-only");
+    if (this.#isPlaying) {
+      this.stop();
+      this.#isPlaying = false;
+      this.classList.remove("is-playing");
+      label.textContent = "Play metronome";
+    } else {
+      this.start();
+      this.#isPlaying = true;
+      this.classList.add("is-playing");
+      label.textContent = "Pause metronome";
+    }
+  }
+
+  /**
    * Starts the metronome.
    */
   start() {
-    if (this.#isPlaying) {
-      return;
-    }
-
-    console.log(1)
+    // if (this.#isPlaying) {
+    //   return;
+    // }
 
     /** @type {number} */
     let beatCount = 0;
@@ -98,9 +117,9 @@ export default class GuitarMetronome extends HTMLElement {
    * Stops the metronome.
    */
   stop() {
-    if (!this.#isPlaying) {
-      return;
-    }
+    // if (!this.#isPlaying) {
+    //   return;
+    // }
 
     clearInterval(this.#timerId);
     this.#timerId = null;
@@ -112,7 +131,7 @@ export default class GuitarMetronome extends HTMLElement {
    */
   setBpm(bpm) {
     if (bpm <= 0 || isNaN(bpm)) {
-      throw new Error('Invalid BPM value. Must be a number greater than 0.');
+      throw new Error("Invalid BPM value. Must be a number greater than 0.");
     }
 
     this.#bpm = bpm;
@@ -125,6 +144,7 @@ export default class GuitarMetronome extends HTMLElement {
   render() {
     this.root.innerHTML = `
       <style>${css}</style>
+      <div class="container">
       <div class="signature">
         <button class="signature" data-signature="3/4">
           <div>2</div>
@@ -139,29 +159,34 @@ export default class GuitarMetronome extends HTMLElement {
           <div>8</div>
         </button>
       </div>
-      <div>
-        <div class="tempo">120</div>
-        <div class="bpm">bpm</div>
-      </div>
       <div class="beats-indicators">
         <div class="beat"></div>
         <div class="beat"></div>
         <div class="beat"></div>
       </div>
-      <div class="controls">
-        <button class="control start" aira-label="Start the metronome" onclick="this.getRootNode().host.start()">Start</button>
+      <div class="tempo-wrapper">
+      <button class="tempo-control tempo-control--decrease" aria-label="Decrease tempo">
+      <svg role="img" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <path fill="currentColor" d="M19 12.998H5v-2h14z"/>
+</svg>
+        <span class="sr-only">Decrease tempo</span>
+            </button>
+          <div class="tempo" data-text="bpm">120</div>
+        <button class="tempo-control tempo-control--increase" aria-label="Increase tempo">
+        <svg role="img" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <path fill="currentColor" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"/>
+</svg>
+          <span class="sr-only">Increase tempo</span>
+          </button>
       </div>
+      
       <div class="controls">
-        <button class="control decrement" aria-label="Decrease tempo">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M19 12.998H5v-2h14z"/>
-          </svg>
+        <button class="toggle-play" type="button" onclick="this.getRootNode().host.toggle(this)">
+          <svg class="play-icon" fill="currentColor role="img" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
+          <svg class="pause-icon" role="img" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>
+          <span class="sr-only">Play metronome</span>
         </button>
-        <button class="control increment" aria-label="Increase tempo">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path fill="currentColor" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"/>
-          </svg>
-        </button>
+      </div>
       </div>
     `;
   }
